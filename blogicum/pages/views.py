2606b508c73e636f149a5012ed517aabd0c430b8
Lev Korobeinikov <korobeinikoffl@yandex.ru>
2024-03-12 23:33:08 +0500

    django pro

---
+++ blogicum/pages/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+from django.shortcuts import render
+
+from django.shortcuts import render
+
+
+def about(request):
+    template = 'pages/about.html'
+    return render(request, template)
+
+
+def rules(request):
+    template = 'pages/rules.html'
+    return render(request, template)
+
+# Create your views here.
